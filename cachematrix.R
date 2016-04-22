a432c67598259a71d7702d6b06c790ba917056f4
tusijing <tusijing007@gmail.com>
2016-04-22 17:52:14 +0800

    Add all the missing messages needed.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## make CacheMarix Creates a special List, this List in fact contains 4 functions:
+## set the Matrix
+## get the Matrix
+## set the inv of the matrix
+## get the inv of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    m<-NULL
+    set<-function(){
+        x<<-y
+        m<<-NULL
+    }
+    get <- function() x
+    setinv <- function(inv) m<<-inv
+    getinv <- function() m
+    list(set = set, get = get, setinv = setinv, getinv =getinv)
 }
 
 
-## Write a short comment describing this function
+## the cachheSolve first tries to see if the inv has already been stored
+## if its true, return the cached data
+## if not, use the function "solve"
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m<-x$getinv()
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    data <-x$get()
+    m<-solve(data, ...)
+    x$setinv(m)
+    m
 }
